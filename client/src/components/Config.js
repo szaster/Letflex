8e0dc33fa9e9033b618ea827b67de289d5132bea
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-08-28 00:17:04 -0500

    API calls with landingPage showing movies

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = '04e8ba6c0782fe40668e71c3a4bc5eca';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
