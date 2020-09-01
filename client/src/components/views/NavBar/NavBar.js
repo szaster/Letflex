2b3ba5fefc73f35dc5daf0b2fc228863409ed9d8
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-09-01 01:29:30 -0500

    structure project files to match class structure

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,7 +1,44 @@
+// import React, { Component } from 'react'
+// import { Menu, Segment } from 'semantic-ui-react'
+
+// export default class MenuExampleInvertedSecondary extends Component {
+//   state = { activeItem: 'home' }
+
+//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+//   render() {
+//     const { activeItem } = this.state
+
+//     return (
+//       <Segment inverted>
+//         <Menu inverted pointing secondary>
+//           <Menu.Item
+//             name='home'
+//             active={activeItem === 'home'}
+//             onClick={this.handleItemClick}
+//           />
+//           <Menu.Item
+//             name='messages'
+//             active={activeItem === 'messages'}
+//             onClick={this.handleItemClick}
+//           />
+//           <Menu.Item
+//             name='friends'
+//             active={activeItem === 'friends'}
+//             onClick={this.handleItemClick}
+//           />
+//         </Menu>
+//       </Segment>
+//     )
+//   }
+// }
+
+
 import React, { useState } from 'react';
 import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
 import { Drawer, Button, Icon } from 'antd';
+import { Menu, Segment } from 'semantic-ui-react'
 import './Sections/Navbar.css';
 
 function NavBar() {
@@ -51,3 +88,4 @@ function NavBar() {
 }
 
 export default NavBar
+
