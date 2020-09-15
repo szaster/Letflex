4073dc09e7a7b8dc2436a8cb8c021da774efddbe
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-09-14 19:10:19 -0500

    Banner

--- client/src/components/commons/Banner.js
+++ client/src/components/commons/Banner.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
-import { requests } from "../../components/Config";
+import { requests } from "../Config";
 import { Button } from "semantic-ui-react";
+import "./Banner.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -12,30 +15,30 @@ function Banner() {
       const request = await axios.get(requests.fetchActionMovies);
       setMovie(
         request.data.results[
-          Math.floor(Math.random() * request.data.results.lenth - 1)
+          Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
-      return request;
+      //return request;
     }
     fetchData();
   }, []);
-  console.log(movie);
   return (
     <header
       className="banner"
       style={{
+        color: 'white',
         backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmbd.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundImage: `url("${base_url}${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 style={{color: "white",}}>{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="banner_buttons">
           <Button className="banner_button">Play</Button>
           <Button className="banner_button">My List</Button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 style={{color: "white"}} className="banner_description">{movie?.overview}</h1>
       </div>
     </header>
   );
