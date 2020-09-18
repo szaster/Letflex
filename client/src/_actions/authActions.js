d8a00ced797598a3a0fa2e395eefc72bdcc6cd43
Svitlana Zaster <szaster@protonmail.com>
2020-09-17 20:14:10 -0500

    Corrected set user action

--- client/src/_actions/authActions.js
+++ client/src/_actions/authActions.js
@@ -1,19 +1,3 @@
-import axios from "axios";
-
 import { SET_CURRENT_USER } from "./types";
 
-export const setCurrentUser = () => (dispatch) => {
-  axios
-    .get("/api/current_user")
-    .then((res) => {
-      //this.setState({ profile: res.data });
-      console.log(res.data);
-      dispatch({
-        type: SET_CURRENT_USER,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      console.log(err.response);
-    });
-};
+export const setCurrentUser = { type: SET_CURRENT_USER };
