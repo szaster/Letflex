316259766c3c527481202a7280d2de144e2c4d49
Carina Reyes <cereyes1792@gmail.com>
2020-09-19 10:59:41 -0500

    fixed bugs

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Row, Button } from "antd";
 import axios from "axios";
-import Banner from '../../commons/Banner';
+import Banner from "../../commons/Banner";
 import Row from "../../commons/Row";
 import "../../commons/Row.css";
 import Comments from "./Sections/Comments";
@@ -23,7 +22,6 @@ import { Grid, Button } from "semantic-ui-react";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function MovieDetailPage(props) {
-
   useEffect(() => {
     async function fetchData() {
       // pick random movie for
@@ -37,61 +35,56 @@ function MovieDetailPage(props) {
     }
     fetchData();
   }, []);
-  const movieId = props.match.params.movieId
-    const [Movie, setMovie] = useState([])
-    const [Casts, setCasts] = useState([])
-    const [CommentLists, setCommentLists] = useState([])
-    const [LoadingForMovie, setLoadingForMovie] = useState(true)
-    const [LoadingForCasts, setLoadingForCasts] = useState(true)
-    const [ActorToggle, setActorToggle] = useState(false)
-    const movieVariable = {
-        movieId: movieId
-    }
+  const movieId = props.match.params.movieId;
+  const [Movie, setMovie] = useState([]);
+  const [Casts, setCasts] = useState([]);
+  const [CommentLists, setCommentLists] = useState([]);
+  const [LoadingForMovie, setLoadingForMovie] = useState(true);
+  const [LoadingForCasts, setLoadingForCasts] = useState(true);
+  const [ActorToggle, setActorToggle] = useState(false);
+  const movieVariable = {
+    movieId: movieId,
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    let endpointForMovieInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+    fetchDetailInfo(endpointForMovieInfo);
 
-        let endpointForMovieInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`;
-        fetchDetailInfo(endpointForMovieInfo)
+    axios.post("/api/comment/getComments", movieVariable).then((response) => {
+      console.log(response);
+      if (response.data.success) {
+        console.log("response.data.comments", response.data.comments);
+        setCommentLists(response.data.comments);
+      } else {
+        alert("Failed to get comments Info");
+      }
+    });
+  }, []);
 
-        axios.post('/api/comment/getComments', movieVariable)
-            .then(response => {
-                console.log(response)
-                if (response.data.success) {
-                    console.log('response.data.comments', response.data.comments)
-                    setCommentLists(response.data.comments)
-                } else {
-                    alert('Failed to get comments Info')
-                }
-            })
+  const toggleActorView = () => {
+    setActorToggle(!ActorToggle);
+  };
 
-    }, [])
+  const fetchDetailInfo = (endpoint) => {
+    fetch(endpoint)
+      .then((result) => result.json())
+      .then((result) => {
+        console.log(result);
+        setMovie(result);
+        setLoadingForMovie(false);
 
-    const toggleActorView = () => {
-        setActorToggle(!ActorToggle)
-    }
+        let endpointForCasts = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+        fetch(endpointForCasts)
+          .then((result) => result.json())
+          .then((result) => {
+            console.log(result);
+            setCasts(result.cast);
+          });
 
-    const fetchDetailInfo = (endpoint) => {
-
-        fetch(endpoint)
-            .then(result => result.json())
-            .then(result => {
-                console.log(result)
-                setMovie(result)
-                setLoadingForMovie(false)
-
-                let endpointForCasts = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
-                fetch(endpointForCasts)
-                    .then(result => result.json())
-                    .then(result => {
-                        console.log(result)
-                        setCasts(result.cast)
-                    })
-
-                setLoadingForCasts(false)
-            })
-            .catch(error => console.error('Error:', error)
-            )
-    }
+        setLoadingForCasts(false);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 
   const updateComment = (newComment) => {
     setCommentLists(CommentLists.concat(newComment));
@@ -99,29 +92,32 @@ function MovieDetailPage(props) {
 
   return (
     <div style={{ padding: "3rem" }}>
-     <NavBar />
-       {/* <VideoPlayer /> */}
-      {/* Header */}  
+      <NavBar />
+      {/* <VideoPlayer /> */}
+      {/* Header */}
       <header
-      className="banner"
-      style={{
-        color: 'white',
-        backgroundSize: "cover",
-        backgroundImage: `url("${base_url}${Movie?.backdrop_path}")`,
-        backgroundPosition: "center center",
-      }}
-    >
-      <div className="banner_contents">
-        <h1 className='banner_title'>{Movie?.title || Movie?.name || Movie?.original_name}</h1>
-        <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">Watch Trailer</button>
+        className="banner"
+        style={{
+          color: "white",
+          backgroundSize: "cover",
+          backgroundImage: `url("${base_url}${Movie?.backdrop_path}")`,
+          backgroundPosition: "center center",
+        }}
+      >
+        <div className="banner_contents">
+          <h1 className="banner_title">
+            {Movie?.title || Movie?.name || Movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button className="banner_button">Play</button>
+            <button className="banner_button">Watch Trailer</button>
+          </div>
+          <h1 style={{ color: "white" }} className="banner_descriptionD">
+            {Movie?.overview}
+          </h1>
         </div>
-        <h1 style={{color: "white"}} className="banner_descriptionD">{Movie?.overview}</h1>
-      </div>
-      <div className='banner-fadeBottom' />
-      
-    </header>
+        <div className="banner-fadeBottom" />
+      </header>
 
       {/* Body */}
       <div style={{ margin: "1rem 4rem" }}>
@@ -176,7 +172,10 @@ function MovieDetailPage(props) {
           />
         </div>
       </div>
-      <Row title="Similar Movies" fetchUrl={`/movie/${movieId}/similar?api_key=${API_KEY}&language=en-US&page=1`} />
+      <Row
+        title="Similar Movies"
+        fetchUrl={`/movie/${movieId}/similar?api_key=${API_KEY}&language=en-US&page=1`}
+      />
     </div>
   );
 }
