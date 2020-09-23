34b6d76236bce3de393124bc83eb524556effa48
Svitlana Zaster <szaster@protonmail.com>
2020-09-23 16:53:55 -0500

    initial commit

--- routes/commentsNew.js
+++ routes/commentsNew.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const ensureAuth = require("../middleware");
+const { Comment } = require("../models/Comment");
 const router = express.Router();
 
 // const { User } = require("../models/User");
 
 router.get("/", [ensureAuth], (req, res) => {
-  const { id } = req.user;
+  Comment.find()
+    .populate("author")
+    .sort({ createdAt: -1 })
+    .then((comments) => res.json(comments));
   res.status(501).send();
+  //
+  //   const { id } = req.user;
+  //   res.status(501).send();
+});
+
+router.post("/saveComment", [ensureAuth], (req, res) => {
+  const comment = new Comment(req.body);
 });
 
 module.exports = router;
