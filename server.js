ed35dd5f11e2decff88a680cb76b64f52bbdf373
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-09-23 20:44:41 -0500

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cookieSession = require("cookie-session");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: false }));
