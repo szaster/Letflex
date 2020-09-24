8150ac98ea528bb9afce0c9265294e1ed423ea37
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-09-23 20:06:55 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -47,7 +51,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-<<<<<<< HEAD
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/letflex", {
 //   useUnifiedTopology: true,
@@ -55,8 +58,6 @@ if (process.env.NODE_ENV === "production") {
 //   force: true,
 // });
 
-=======
->>>>>>> 51201b2b643ad5cf41e7aea5b5ae302169b80993
 // Start the API server
 app.listen(PORT, function (err) {
   if (err) throw err;
