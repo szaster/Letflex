cdbd003151df11d0a743cc8bf48b06790b6b556a
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-09-26 20:07:57 -0500

    added link to person's detail page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,63 +8,81 @@ import MainNavbar from "../NavBar/MainNavbar";
 import { Link } from "react-router-dom";
 
 function LandingPage() {
+	const [persons, setPersons] = useState([]);
 
-  const [persons, setPersons] = useState([]);
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setPersons(await fetchPersons());
+		};
 
-  useEffect(() => {
-    const fetchAPI = async () => {
-      setPersons(await fetchPersons());
-    };
+		fetchAPI();
+	}, []);
 
-    fetchAPI();
-  }, []);
-
-  return (
-    <div className="">
-      <MainNavbar />
-      <div style={{ paddingBottom: "5rem", paddingTop: "3rem" }}>
-        {/* Nav */}
-        <Banner />
-        <Row
-          title="NETFLIX ORIGINALS"
-          fetchUrl={requests.fetchUpComing}
-          isLargeRow
-        />
-        <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
-        <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
-        <Row title="POPULAR MOVIES" fetchUrl={requests.fetchPopularMovies} />
-        <Row title="POPULAR TV SHOWS" fetchUrl={requests.fetchPopularTVShows} />
-        <Row title="TV SHOWS" fetchUrl={requests.fetchTVShows} />
-        <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
-        <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
-        <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
-        <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
-        <div>
-        <h2 className="row">Trending People</h2>
-        <div className="row_posters">
-        {persons.map((p, i) => (
-      <div className="col-md-3 text-center" key={i}>
-        <Link exact to={`/credits/${p.id}`}>
-        <img
-          className="img-fluid rounded-circle mx-auto d-block"
-          src={p.profileImg}
-          alt={p.name}
-        />
-        <p className="font-weight-bold text-center">{p.name}</p>
-        <p
-          className="font-weight-light text-center"
-          style={{ color: "#5a606b" }}
-        >
-          Trending for {p.known}
-        </p>
-        </Link>
-      </div>
-  ))}
-  </div>
-      </div>
-    </div>
-    </div>
-  );
+	return (
+		<div className=''>
+			<MainNavbar />
+			<div style={{ paddingBottom: "5rem", paddingTop: "3rem" }}>
+				{/* Nav */}
+				<Banner />
+				<Row
+					title='NETFLIX ORIGINALS'
+					fetchUrl={requests.fetchUpComing}
+					isLargeRow
+				/>
+				<Row title='TRENDING NOW' fetchUrl={requests.fetchTrending} />
+				<Row title='TOP RATED' fetchUrl={requests.fetchTopRated} />
+				<Row
+					title='POPULAR MOVIES'
+					fetchUrl={requests.fetchPopularMovies}
+				/>
+				<Row
+					title='POPULAR TV SHOWS'
+					fetchUrl={requests.fetchPopularTVShows}
+				/>
+				<Row title='TV SHOWS' fetchUrl={requests.fetchTVShows} />
+				<Row
+					title='COMEDY MOVIES'
+					fetchUrl={requests.fetchComedyMovies}
+				/>
+				<Row
+					title='HORROR MOVIES'
+					fetchUrl={requests.fetchHorrorMovies}
+				/>
+				<Row
+					title='ROMANCE MOVIES'
+					fetchUrl={requests.fetchRomanceMovies}
+				/>
+				<Row
+					title='DOCUMENTARIES'
+					fetchUrl={requests.fetchDocumentaries}
+				/>
+				<div>
+					<h2 className='row'>Trending People</h2>
+					<div className='row_posters'>
+						{persons.map((p, i) => (
+							<div className='col-md-3 text-center' key={i}>
+								<Link exact to={`/credits/${p.id}`}>
+									<img
+										className='img-fluid rounded-circle mx-auto d-block'
+										src={p.profileImg}
+										alt={p.name}
+									/>
+									<p className='font-weight-bold text-center'>
+										{p.name}
+									</p>
+									<p
+										className='font-weight-light text-center'
+										style={{ color: "#5a606b" }}>
+										Trending for {p.known}
+									</p>
+								</Link>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LandingPage;
