71473ad1e9174404c46b612540bb3cb0104d7583
Svitlana Zaster <szaster@protonmail.com>
2020-09-27 20:26:38 -0500

    wip

--- routes/favorite.js
+++ routes/favorite.js
@@ -5,11 +5,9 @@ const { Favorite } = require("../models/Favorite");
 
 router.get("/all", async (req, res) => {
   try {
-    const { movieId } = req.query;
-    const data = await Favorite.find({ movieId });
     //.populate({ path: "user" })
     // .sort({ createdAt: -1 });
-    res.status(200).json(data);
+    res.status(501).send();
   } catch (error) {
     res.status(400).json({ message: error.message }).send();
   }
@@ -17,6 +15,8 @@ router.get("/all", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
+    const { movieId } = req.query;
+    const data = await Favorite.find({ movieId });
     res.status(501).send();
   } catch (error) {
     res.status(400).json({ message: error.message }).send();
